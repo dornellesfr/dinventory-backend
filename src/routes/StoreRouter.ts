9eaf3b31f705434f1f16544d7118a670e6d1db52
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-05-03 16:12:48 -0300

    fix: word put although patch

--- src/routes/StoreRouter.ts
+++ src/routes/StoreRouter.ts
@@ -6,7 +6,7 @@ const storeController = new StoreController();
 
 storeRouter.get('/', ((req, res) => storeController.findAll(req, res) as unknown) as RequestHandler);
 storeRouter.post('/', ((req, res) => storeController.create(req, res) as unknown) as RequestHandler);
-storeRouter.patch('/', ((req, res) => storeController.update(req, res) as unknown) as RequestHandler);
+storeRouter.put('/', ((req, res) => storeController.update(req, res) as unknown) as RequestHandler);
 storeRouter.delete('/', ((req, res) => storeController.removeStore(req, res) as unknown) as RequestHandler);
 
 export default storeRouter;
