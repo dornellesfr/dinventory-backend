b45f2ca1e7e7beec9c592b33c886f1da1761ffc9
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-05-04 20:21:58 -0300

    feat: add admin verification to routes that controll stores registered

--- src/routes/StoreRouter.ts
+++ src/routes/StoreRouter.ts
@@ -1,17 +1,30 @@
 import { type RequestHandler, Router } from 'express';
 import StoreController from '../controllers/StoreController';
 import validateInputsStore from '../middlewares/validateInputStore';
+import validateAdminUser from '../middlewares/authAdmin';
 
 const storeRouter = Router();
 const storeController = new StoreController();
 
-storeRouter.get('/', ((req, res) => storeController.findAll(req, res) as unknown) as RequestHandler);
+storeRouter.get('/',
+  (validateAdminUser as RequestHandler),
+  ((req, res) => storeController.findAll(req, res) as unknown) as RequestHandler
+);
 
-storeRouter.post('/', validateInputsStore,
-  ((req, res) => storeController.create(req, res) as unknown) as RequestHandler);
+storeRouter.post('/',
+  (validateAdminUser as RequestHandler),
+  validateInputsStore,
+  (((req, res) => storeController.create(req, res) as unknown) as RequestHandler)
+);
 
-storeRouter.put('/', ((req, res) => storeController.update(req, res) as unknown) as RequestHandler);
+storeRouter.put('/',
+  (validateAdminUser as RequestHandler),
+  ((req, res) => storeController.update(req, res) as unknown) as RequestHandler
+);
 
-storeRouter.delete('/', ((req, res) => storeController.removeStore(req, res) as unknown) as RequestHandler);
+storeRouter.delete('/',
+  (validateAdminUser as RequestHandler),
+  ((req, res) => storeController.removeStore(req, res) as unknown) as RequestHandler
+);
 
 export default storeRouter;
